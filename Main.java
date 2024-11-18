48f5cbc0ea2ec346db78fff59a28751284f56654
Priyansh Porwal <priyanshporwal28@gmail.com>
2024-11-18 23:02:34 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        QuestionServer server= new QuestionServer();
+        server.playQuiz();
+        server.printScore();
+    }
+}
---
+++ Question.java
@@ -0,0 +1,61 @@
+public class Question
+{
+    private int id;
+    private String question;
+    private String opt1;
+    private String opt2;
+    private String opt3;
+    private String opt4;
+    private String answer;
+
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String answer)
+    {
+        this.id= id;
+        this.question= question;
+        this.opt1=opt1;
+        this.opt2=opt2;
+        this.opt3=opt3;
+        this.opt4=opt4;
+        this.answer=answer;
+    }
+    public int getId()
+    {
+        return id;
+    }
+    public String getQuestion()
+    {
+        return question;
+    }
+    public String getOpt1()
+    {
+        return opt1;
+    }
+    public String getOpt2()
+    {
+        return opt2;
+    }
+    public String getOpt3()
+    {
+        return opt3;
+    }
+    public String getOpt4()
+    {
+        return opt4;
+    }
+    public String getAnswer()
+    {
+        return answer;
+    }
+    public String toString()
+    {
+        return "Question{" +
+                "id=" + id +
+                ", question='" + question + '\'' +
+                ", opt1='" + opt1 + '\'' +
+                ", opt2='" + opt2 + '\'' +
+                ", opt3='" + opt3 + '\'' +
+                ", opt4='" + opt4 + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
+}
